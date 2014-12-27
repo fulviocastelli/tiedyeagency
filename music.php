2cdb8227c794eb70660781f62be0cc708c65bd5e
susannalindskog <susannalindskog@Susannas-MacBook-Pro.local>
2014-12-27 11:52:06 +0100

    changes

--- music.php
+++ music.php
@@ -12,17 +12,10 @@ $title = "Music | TIE DYE";
 		<img class="img-responsive visible-xs hidden-sm hidden-md hidden-lg" src="media/phone001.jpg">
 
 		<section>
-			<h1>Hej</h1>
-		</section>
-        <!--<div class="event container-fluid">
-                    <br>
-                    <br>
-                    <br>
-                    <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
-                    </div>
+			    <div class="event container-fluid">
                     <div class="row">
                         <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
-                            <img class="img-responsive" src="/media/music/basinski.jpg">
+                            <img class="img-responsive" src="media/music/basinski.jpg">
                         </div>
                     </div>
                     <div class=" col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-12 col-xs-offset-0">
@@ -36,10 +29,12 @@ $title = "Music | TIE DYE";
                          </div>
                         <div class="text-justify">
                             <p class="event-desc"> All'interno di un'installazione appositamente concepita da Nico Vascellari ospiti quali Arto Lindsay, William Basinski, Marina Abramovic, Luigi Ontani, Bjorn Copeland, Filipa Ramos, Prurient, Luca Lo Pinto, Vatican Shadow e molti altri. </p>
-                           <!-- <p class="event-desc">line break text.</p>-->
-                       <!-- </div>  
+                           <p class="event-desc">line break text.</p>
+                       </div>  
                     </div>
                 </div>
+		</section>
+
 
 
         
